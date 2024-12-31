64df5b398d254f4df8ea37ad771ab48365e2e1bf
Ronan Russel Andal <rrandal@hawaii.edu>
2024-12-31 13:41:32 -1000

    read me

--- app/about.html/ClientPage.tsx
+++ app/about.html/ClientPage.tsx
@@ -7,7 +7,7 @@ import Layout from '@/components/Layout';
 const MainContent = () => {
     return (
         <>
-            <p>This web app is a project I created to showcase everything I’ve learned from a software engineering class. It’s also inspired by my uncle, who is a Systems Administrator at Hawaiian Airlines, and who also told me my projects were garbage lol. My previous experience working at Target gave me the idea—there, we had an app that displayed our shifts, allowed us to pick up shifts dropped by others, and provided a wealth of other helpful features. I wanted to bring that functionality to life using my newfound software engineering skills.
+            <p>This web app is a project I created to showcase everything I’ve learned throughout my software engineering journey. It’s also inspired by my uncle, who is a Systems Administrator at Hawaiian Airlines, and who also told me my projects were garbage lol. My previous experience working at Target gave me the idea—there, we had an app that displayed our shifts, allowed us to pick up shifts dropped by others, and provided a wealth of other helpful features. I wanted to bring that functionality to life using my newfound software engineering skills.
             </p>
    
         </>
